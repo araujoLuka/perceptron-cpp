7d2fcfce9a05a77ac052c27d93a4518844ad7653
Lucas Araujo (DSBD) <lucasaraujo1@ufpr.br>
2024-02-22 17:39:44 -0300

    updated with optimization and standardization

--- Perceptron.cpp
+++ Perceptron.cpp
@@ -4,20 +4,26 @@
 
 #include "Perceptron.hpp"
 
-#include <iomanip>
-#include <iostream>
+#include <cstdlib>
 
 namespace ml {
 
 Perceptron::Perceptron(const float learningRate, const int inputSize)
-    : learningRate{learningRate}, inputSize{inputSize}, totalEpochs{0}, biasWeight{0.f} {
+    : learningRate{learningRate},
+      inputSize{inputSize},
+      totalEpochs{0},
+      biasWeight{1.f} {
     // Initialize weights with random values between 0 and 1
     for (int i{0}; i < this->inputSize; ++i)
         this->weights.push_back((float)rand() / (float)RAND_MAX);
 }
 
-Perceptron::Perceptron(const float learningRate, const int inputSize, const std::vector<float>& weights)
-    : learningRate{learningRate}, inputSize{inputSize}, totalEpochs{0}, biasWeight{0.f} {
+Perceptron::Perceptron(const float learningRate, const int inputSize,
+                       const std::vector<float>& weights)
+    : learningRate{learningRate},
+      inputSize{inputSize},
+      totalEpochs{0},
+      biasWeight{1.f} {
     // Initialize weights with the given values
     for (int i{0}; i < this->inputSize; ++i)
         this->weights.push_back(weights[i]);
@@ -33,68 +39,55 @@ const float Perceptron::activation(const std::vector<float>& input) {
     float scalarProduct{0.f};
 
     // Calculate the scalar product of inputs and weights
-    for (int i{0}; i < this->inputSize; ++i) 
+    for (int i{0}; i < this->inputSize; ++i)
         scalarProduct += input[i] * this->weights[i];
 
     return scalarProduct + this->biasWeight;
 }
 
-const int Perceptron::predict(const std::vector<float>& input) { 
-    return this->activation(input) >= 0 ? 1 : -1; 
+const int Perceptron::predict(const std::vector<float>& input) {
+    return this->activation(input) >= 0 ? 1 : -1;
 }
 
-const int Perceptron::predict(const float scalarProduct) { 
-    return scalarProduct >= 0 ? 1 : -1; 
+const int Perceptron::predict(const float scalarProduct) {
+    return scalarProduct >= 0 ? 1 : -1;
 }
 
-void Perceptron::fit(const std::vector<std::vector<float>>& trainingData, const std::vector<int>& labels, const int epochs) {
+void Perceptron::fit(const std::vector<std::vector<float>>& trainingData,
+                     const std::vector<int>& labels, const int epochs) {
     for (int epoch{0}; epoch < epochs; ++epoch) {
-        // Visualize the training process
-        std::cout << "Epoch: " << epoch + 1 << '/' << epochs << " | ";
-        std::cout << "Initial weights: ";
-        std::cout << std::fixed << std::setw(9) << std::setprecision(5) 
-            << this->biasWeight << ' ';
-        for (int i{0}; i < this->inputSize; ++i)
-            std::cout << std::fixed << std::setw(9) << std::setprecision(5) 
-                << this->weights[i] << ' ';
-        std::cout << "| ";
-
         // Count the number of misclassified samples
-        // If the count is 0, the perceptron has learned, and we can stop training
+        // If the count is 0, the perceptron has learned, and we can stop
+        // training
         int misclassifiedCount{0};
+
+        // Iterate over the training data
         for (std::size_t i{0}; i < trainingData.size(); ++i) {
+            int label{labels[i]};
             float scalarProduct{this->activation(trainingData[i])};
             int prediction{this->predict(scalarProduct)};
-            int label{labels[i]};
-            // Using criterion as suggested by Bishop
-            // Only update weights if prediction is wrong
+
+            // Update weights only if prediction is wrong
             if (prediction != label) {
-                misclassifiedCount++;
-                // We can multiply the learning rate by the label, and the operation will be inverted to the correct one
-                // If the label is 1, the learning rate will be positive; otherwise, it will be negative
+                ++misclassifiedCount;
+
                 float learningRate{this->learningRate * label};
+
+                // Update bias weight
                 this->biasWeight += learningRate;
+
+                // Update weights
                 for (int j{0}; j < this->inputSize; ++j)
                     this->weights[j] += learningRate * trainingData[i][j];
             }
         }
-        // Visualize the training process
-        std::cout << "Misclassified: " << misclassifiedCount << " | ";
-        std::cout << "Final weights: ";
-        std::cout << std::fixed << std::setw(9) << std::setprecision(5) 
-            << this->biasWeight << ' ';
-        for (int i{0}; i < this->inputSize; ++i)
-            std::cout << std::fixed << std::setw(9) << std::setprecision(5) 
-                << this->weights[i] << ' ';
-        std::cout << "\n";
 
         // If the perceptron has learned, we can stop training
         if (misclassifiedCount == 0) {
-            this->totalEpochs = epoch + 1; // +1 because the epoch starts at 0
+            this->totalEpochs = epoch + 1;  // +1 because the epoch starts at 0
             break;
         }
     }
 }
 
 }  // namespace ml
-
--- PerceptronSIMD.cpp
+++ PerceptronSIMD.cpp
@@ -4,20 +4,25 @@
 
 #include "PerceptronSIMD.hpp"
 
-#include <iostream>
-#include <iomanip>
-
 namespace ml {
 
 PerceptronSIMD::PerceptronSIMD(const float learningRate, const int inputSize)
-    : learningRate{learningRate}, inputSize{inputSize}, totalEpochs{0}, biasWeight{0.f} {
+    : learningRate{learningRate},
+      inputSize{inputSize},
+      totalEpochs{0},
+      biasWeight{1.f} {
     // Initialize weights with random values between 0 and 1
-    this->weightsSIMD = _mm_set_ps((float)rand() / (float)RAND_MAX, (float)rand() / (float)RAND_MAX,
-                                   (float)rand() / (float)RAND_MAX, (float)rand() / (float)RAND_MAX);
+    this->weightsSIMD = _mm_set_ps(
+        (float)rand() / (float)RAND_MAX, (float)rand() / (float)RAND_MAX,
+        (float)rand() / (float)RAND_MAX, (float)rand() / (float)RAND_MAX);
 }
 
-PerceptronSIMD::PerceptronSIMD(const float learningRate, const int inputSize, const std::vector<float>& weights)
-    : learningRate{learningRate}, inputSize{inputSize}, totalEpochs{0}, biasWeight{0.f} {
+PerceptronSIMD::PerceptronSIMD(const float learningRate, const int inputSize,
+                               const std::vector<float>& weights)
+    : learningRate{learningRate},
+      inputSize{inputSize},
+      totalEpochs{0},
+      biasWeight{1.f} {
     // Initialize weights with the given values
     this->weightsSIMD = _mm_load_ps(&weights[0]);
 }
@@ -58,63 +63,54 @@ const float PerceptronSIMD::activation(const __m128& input) {
 }
 
 // Prediction functions
-const int PerceptronSIMD::predict(const std::vector<float>& input) { return this->activation(input) >= 0 ? 1 : -1; }
+const int PerceptronSIMD::predict(const std::vector<float>& input) {
+    return this->activation(input) >= 0 ? 1 : -1;
+}
 
-const int PerceptronSIMD::predict(const float scalarProduct) { return scalarProduct >= 0 ? 1 : -1; }
+const int PerceptronSIMD::predict(const float scalarProduct) {
+    return scalarProduct >= 0 ? 1 : -1;
+}
 
 // Training function
-void PerceptronSIMD::fit(const std::vector<std::vector<float>>& trainingData, const std::vector<int>& labels,
-                         const int epochs) {
+void PerceptronSIMD::fit(const std::vector<std::vector<float>>& trainingData,
+                         const std::vector<int>& labels, const int epochs) {
     for (int epoch{0}; epoch < epochs; ++epoch) {
-        // Visualize the training process
-        std::cout << "Epoch: " << epoch + 1 << '/' << epochs << " | ";
-        std::cout << "Initial weights: ";
-        std::cout << std::fixed << std::setw(9) << std::setprecision(5) 
-            << this->biasWeight << ' ';
-        for (int i{0}; i < this->inputSize; ++i)
-            std::cout << std::fixed << std::setw(9) << std::setprecision(5) 
-                << this->weightsSIMD[i] << ' ';
-        std::cout << "| ";
-
         // Count the number of misclassified samples
-        // If the count is 0, the perceptron has learned and we can stop training
+        // If the count is 0, the perceptron has learned and we can stop
+        // training
         int misclassifiedCount{0};
 
-        // Process floats in parallel using SIMD
+        // Iterate over the training data
         for (std::size_t i{0}; i < trainingData.size(); ++i) {
+            // Convert input vector to SIMD format
             __m128 input{_mm_load_ps(&trainingData[i][0])};
+
+            int label{labels[i]};
             float scalarProduct{this->activation(input)};
             int prediction{this->predict(scalarProduct)};
-            int label{labels[i]};
 
             // Update weights only if prediction is wrong
             if (prediction != label) {
                 ++misclassifiedCount;
-                // Update bias weight and weights using SIMD operations
+
                 float learningRate{this->learningRate * label};
+
+                // Update bias weight
                 this->biasWeight += learningRate;
+
+                // Update weights using SIMD
                 __m128 lr{_mm_set1_ps(learningRate)};
-                this->weightsSIMD = _mm_add_ps(this->weightsSIMD, _mm_mul_ps(lr, input));
+                this->weightsSIMD =
+                    _mm_add_ps(this->weightsSIMD, _mm_mul_ps(lr, input));
             }
         }
 
-        // Visualize the training process
-        std::cout << "Misclassified: " << misclassifiedCount << " | ";
-        std::cout << "Final weights: ";
-        std::cout << std::fixed << std::setw(9) << std::setprecision(5) 
-            << this->biasWeight << ' ';
-        for (int i{0}; i < this->inputSize; ++i)
-            std::cout << std::fixed << std::setw(9) << std::setprecision(5) 
-                << this->weightsSIMD[i] << ' ';
-        std::cout << '\n';
-
         // If the perceptron has learned, we can stop training
         if (misclassifiedCount == 0) {
-            this->totalEpochs = epoch + 1; // +1 because the epoch starts at 0
+            this->totalEpochs = epoch + 1;  // +1 because the epoch starts at 0
             break;
         }
     }
 }
 
 }  // namespace ml
-
